c5cba96a15b9654aa06de57aa48ee8618293ff0e
Laxman_singh <laxmansinghgaria2693@gmail.com>
2019-06-24 21:16:28 +0530

    env file

--- models/transactions.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const { Schema } = mongoose;
-
-const transactionSchema = new Schema({}, { strict: false });
-
-export default mongoose.model('Transaction', transactionSchema);
