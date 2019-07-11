ba25be34742b344138c7b32949994e701c8d1f5b
Laxman_singh <laxmansinghgaria2693@gmail.com>
2019-07-11 14:07:05 +0530

    removal of extra component path

--- models/index.js
+++ models/index.js
@@ -1,5 +1,3 @@
 export { default as User } from './users';
 export { default as Device } from './devices';
 export { default as Content } from './content';
-export { default as Transaction } from './transactions';
-export { default as Fitness } from './fitnessData';
